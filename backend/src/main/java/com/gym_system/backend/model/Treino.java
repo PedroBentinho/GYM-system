0518eca9dce9ccd897c7e4d6ab05c9f7d3361584
gabrieel10 <ghs.lhs.crs2012@gmail.com>
2026-09-23 08:47:56 -0300

    trino

--- backend/src/main/java/com/gym_system/backend/model/Treino.java
+++ backend/src/main/java/com/gym_system/backend/model/Treino.java
@@ -1,37 +1,44 @@
 package com.gym_system.backend.model;
 
-import java.lang.annotation.Inherited;
-
-import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Positive;
 
+// Entity diz ao JPA q essa representa uma 
+// tabela no banco de dados.
     @Entity 
 
+// Define o nome da tabela q vai ser criada.
     @Table(name = "treinos")
-    public class treinos {
-
-        @Id 
+    public class Treino {
+// ID marca essa campo como uma chave primaria 
+         @Id 
+// @GeneratedValue diz q ira gerar id automático
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private long id;
+// @column configura a tabela q sera criada bo banco de dados:
+// nullable = false significa q nao pode ser nulo
+        @Column( nullable = false, length =100 )
+        private String Exercicio;
 
-@Column( nullable = false, length =100 )
-private String Exercicio;
+        @Column( nullable = false )
+        private Integer Series;
 
-@Column( nullable = false )
-private Integer Series;
+        @Column(nullable = false)
+        private Integer repeticoes;
 
-@Column(nullable = false)
-private Integer repeticoes;
+        @Column(length = 50)
+        private String grupoMuscular;
 
-@Column(length = 50)
-private String grupoMuscular 
+        @Column(nullable = false)
+        private boolean feitohoje = false;
+// Construtor vazio para o JPA instanciar a classe internamente
+        public Treino() {
+        
+            
+        }
 
-@Column(nullable = false)
-private boolean feitohoje = false;
-public treinos() }
+}
