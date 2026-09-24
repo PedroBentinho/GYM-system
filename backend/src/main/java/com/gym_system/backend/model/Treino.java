2d4784fc52cff7d4430da1fe20c46de0b30b45c5
Gab2210447 <gabrielalvesdeoliveira250@gmail.com>
2026-09-24 08:36:05 -0300

    Update Treino.java

--- backend/src/main/java/com/gym_system/backend/model/Treino.java
+++ backend/src/main/java/com/gym_system/backend/model/Treino.java
@@ -9,22 +9,22 @@ import jakarta.persistence.Table;
 
 // Entity diz ao JPA q essa representa uma 
 // tabela no banco de dados.
-    @Entity 
+@Entity
 
 // Define o nome da tabela q vai ser criada.
-    @Table(name = "treinos")
-    public class Treino {
-// ID marca essa campo como uma chave primaria 
-         @Id 
-// @GeneratedValue diz q ira gerar id automático
+@Table(name = "treinos")
+public class Treino {
+        // ID marca essa campo como uma chave primaria
+        @Id
+        // @GeneratedValue diz q ira gerar id automático
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private long id;
-// @column configura a tabela q sera criada bo banco de dados:
-// nullable = false significa q nao pode ser nulo
-        @Column( nullable = false, length =100 )
+        // @column configura a tabela q sera criada bo banco de dados:
+        // nullable = false significa q nao pode ser nulo
+        @Column(nullable = false, length = 100)
         private String Exercicio;
 
-        @Column( nullable = false )
+        @Column(nullable = false)
         private Integer Series;
 
         @Column(nullable = false)
@@ -35,10 +35,56 @@ import jakarta.persistence.Table;
 
         @Column(nullable = false)
         private boolean feitohoje = false;
-// Construtor vazio para o JPA instanciar a classe internamente
+
+        // Construtor vazio para o JPA instanciar a classe internamente
         public Treino() {
-        
-            
         }
+
+        public long getId() {
+                return id;
+        }
+
+        public String getExercicio() {
+                return Exercicio;
+        }
+
+        public Integer getSeries() {
+                return Series;
+        }
+
+        public Integer getrepeticoes() {
+                return repeticoes;
+        }
+
+        public String grupoMuscular() {
+                return grupoMuscular;
+        }
+
+        public void setfeitohoje(boolean feitohoje) {
+                this.feitohoje = feitohoje;
+        }
+
+        public void setId(long id){
+                this.id = id;
+        }
+
+        public void setExercicio(String Exercicio){
+                this.Exercicio = Exercicio;
+        }
+
+        public void setSeries(Integer Series){
+                this.Series = Series;
+        }
+
+        public void setrepeticoes(Integer repeticoes) {
+                this.repeticoes = repeticoes;
+        }
+
+        @Override
+        public String toString(){
+                   return "[ id: " + id + " | Exercicio: " + Exercicio + " | Series: " + Series + " | repeticoes: " + repeticoes + " ]";
+        }
+
+
 
 }
